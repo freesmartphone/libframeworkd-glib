9c43c1aa4116925477c481f47db481b6e846fb1c
Klaus Kurzmann <mok@fluxnetz.de>
2009-04-20 12:29:11 +0200

    opimd/sources: add missing dbus connect functions
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/opimd/frameworkd-glib-opimd-dbus.c
+++ src/opimd/frameworkd-glib-opimd-dbus.c
@@ -21,38 +21,54 @@
 #include "frameworkd-glib-opimd-dbus.h"
 #include "frameworkd-glib-opimd-contacts.h"
 #include "frameworkd-glib-opimd-messages.h"
+#include "frameworkd-glib-opimd-sources.h"
 
 
 
 void dbus_connect_to_opimd_contacts(void) {
 	 if(opimdContactsBus == NULL)
-		  opimdContactsBus = dbus_connect_to_interface (OPIMD_BUS, CONTACTS_BUS_PATH, CONTACTS_INTERFACE, "PIM Contacts");
+		  opimdContactsBus = dbus_connect_to_interface (OPIMD_BUS, CONTACTS_BUS_PATH, CONTACTS_INTERFACE, "PIM.Contacts");
 }
 
 DBusGProxy *dbus_connect_to_opimd_contact(const char *contact_path) {
-	 return (dbus_connect_to_interface (OPIMD_BUS, contact_path, CONTACT_INTERFACE, "PIM Contact"));
+	 return (dbus_connect_to_interface (OPIMD_BUS, contact_path, CONTACT_INTERFACE, "PIM.Contact"));
 }
 
 DBusGProxy *dbus_connect_to_opimd_contact_query(const char *query_path) {
-	 return (dbus_connect_to_interface (OPIMD_BUS, query_path, CONTACT_QUERY_INTERFACE, "PIM Contact Query"));
+	 return (dbus_connect_to_interface (OPIMD_BUS, query_path, CONTACT_QUERY_INTERFACE, "PIM.ContactQuery"));
 }
 
 
 void dbus_connect_to_opimd_messages(void) {
 	 if(opimdMessagesBus == NULL)
-		  opimdMessagesBus = dbus_connect_to_interface (OPIMD_BUS, MESSAGES_BUS_PATH, MESSAGES_INTERFACE, "PIM Messages");
+		  opimdMessagesBus = dbus_connect_to_interface (OPIMD_BUS, MESSAGES_BUS_PATH, MESSAGES_INTERFACE, "PIM.Messages");
 }
 
 DBusGProxy *dbus_connect_to_opimd_message(const char *message_path) {
-	 return (dbus_connect_to_interface (OPIMD_BUS, message_path, MESSAGE_INTERFACE, "PIM Message"));
+	 return (dbus_connect_to_interface (OPIMD_BUS, message_path, MESSAGE_INTERFACE, "PIM.Message"));
 }
 
 DBusGProxy *dbus_connect_to_opimd_message_query(const char *query_path) {
-	 return (dbus_connect_to_interface (OPIMD_BUS, query_path, MESSAGE_QUERY_INTERFACE, "PIM Message Query"));
+	 return (dbus_connect_to_interface (OPIMD_BUS, query_path, MESSAGE_QUERY_INTERFACE, "PIM.MessageQuery"));
 }
 
 DBusGProxy *dbus_connect_to_opimd_message_folder(const char *folder_path) {
-	 return (dbus_connect_to_interface (OPIMD_BUS, folder_path, MESSAGE_FOLDER_INTERFACE, "PIM Message Folder"));
+	 return (dbus_connect_to_interface (OPIMD_BUS, folder_path, MESSAGE_FOLDER_INTERFACE, "PIM.MessageFolder"));
 }
 
 		
+
+void dbus_connect_to_opimd_sources(void)
+{
+	if(opimdSourcesBus == NULL)
+		opimdSourcesBus = dbus_connect_to_interface (OPIMD_BUS, SOURCES_BUS_PATH, SOURCES_INTERFACE, "PIM.Sources");
+}
+
+
+DBusGProxy *dbus_connect_to_opimd_source(const char *source_path)
+{
+	return (dbus_connect_to_interface (OPIMD_BUS, source_path, SOURCE_INTERFACE, "PIM.Source"));
+}
+
+
+
--- src/opimd/frameworkd-glib-opimd-dbus.h
+++ src/opimd/frameworkd-glib-opimd-dbus.h
@@ -34,6 +34,11 @@
 #define MESSAGE_QUERY_INTERFACE      "org.freesmartphone.PIM.MessageQuery"
 #define MESSAGE_FOLDER_INTERFACE     "org.freesmartphone.PIM.MessageFolder"
 
+#define SOURCES_BUS_PATH             "/org/freesmartphone/PIM/Sources"
+#define SOURCES_INTERFACE            "org.freesmartphone.PIM.Sources"
+#define SOURCE_INTERFACE             "org.freesmartphone.PIM.Source"
+
+
 
 void dbus_connect_to_opimd_contacts(void);
 DBusGProxy *dbus_connect_to_opimd_contact(const char *contact_path);
@@ -43,6 +48,9 @@ void dbus_connect_to_opimd_messages(void);
 DBusGProxy *dbus_connect_to_opimd_message(const char *message_path);
 DBusGProxy *dbus_connect_to_opimd_message_query(const char *query_path);
 DBusGProxy *dbus_connect_to_opimd_message_folder(const char *folder_path);
+
+void dbus_connect_to_opimd_sources(void);
+DBusGProxy *dbus_connect_to_opimd_source(const char *source_path);
 
 
 #endif
